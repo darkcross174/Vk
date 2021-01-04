dfac217747a21bf9572f113c30d6f798861c3158
Alexey Andreev <dark-cross@rambler.ru>
2021-01-04 23:25:15 +0300

    PostManager

---
+++ src/main/java/manager/PostManager.java
@@ -0,0 +1,18 @@
+package manager;
+
+public class PostManager {
+
+    private PostManager[] posts;
+
+    public PostManager[] searchPost(String query){
+        //логика поиска поста по поисковому запросу
+        return null;
+    }
+
+    public void deletePost(int id){
+        //логика удаления поста по id
+
+    }
+}
+
+
